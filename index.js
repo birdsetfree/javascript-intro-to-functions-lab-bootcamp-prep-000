96a99e00d91cb4fd8cb01f7e0291d1dd500db0b4
Banu Akkuş <akkusbanu92@gmail.com>
2018-10-08 12:07:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function logShout(string) {
 logShout('HELLO')
 
 function sayHiToGrandma(string) {
-  if (string === lowercase)
+  if (string === "lowercase")
   return("I can't hear you!")
-  else if (string === uppercase)
+  else if (string === "uppercase")
   return("YES INDEED!")
   else if (string === "I love you, Grandma.")
   return("I love you, too.")
